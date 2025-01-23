36d376694a04eaa2d1991ef5cfa4daa89c709c71
mandolarson <mirandaephotography@gmail.com>
2025-01-23 11:40:53 -0800

    design push

--- sketch.js
+++ sketch.js
@@ -3,5 +3,29 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background('lightblue');
+  fill('yellow');
+  ellipse(350, 50, 50, 50);
+  fill('lightblue');
+  scale(0.2);
+  bird(0, 0);
+  bird(width/2, height*2);
+  bird(width, height*3);
+  bird(width*2 + 30, height);
+  bird(width*2, height*2);
+  bird(width*3, height*3);
+  bird(width*4, height*4); 
+}
+
+function bird(x, y){
+  push();
+  translate(x, y);
+  beginShape();
+  vertex(0, height);
+  vertex(width/4, 180);
+  vertex(width/2, height);
+  vertex((width/3 * 2), 180);
+  vertex(width, height);
+  endShape();
+  pop();
 }
